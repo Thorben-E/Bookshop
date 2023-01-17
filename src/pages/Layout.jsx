5cebf0ce1bb8a0ba25e0c5c9ef5b6e9022baca71
Thorben <thorbenegberink@gmail.com>
2023-01-17 17:46:51 +0000

    finished

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -4,29 +4,18 @@ import { CartContext } from "./CartContext";
 import { useEffect } from "react";
 
 const Layout = ({cartItems}) => {
-  useEffect(() => {
-    const toggleButton = document.getElementsByClassName('toggle-button')[0]
-    const navbarLinks = document.getElementsByClassName('navlist')[0]
-    toggleButton.addEventListener('click', () => {
-      console.log('click')
-      if (navbarLinks.style.display = 'none') {
-        navbarLinks.style.display = 'block';
-      } else {
-        navbarLinks.style.display = 'none'
-      }
-    })
-  })
-  
+  const [dropdown, setDropdown] = useState(false)
+
   return (
     <>
       <nav>
         <h1>CurrencyShop</h1>
-        <a href="#" className="toggle-button" id="toggle-button">
+        
+        {screen.width < 500 ? <a href="#" className="toggle-button" id="toggle-button" onClick={() => setDropdown((prev) => !prev)}>
           <span className="bar"></span>
           <span className="bar"></span>
           <span className="bar"></span>
-        </a>
-        <div className="navlist">
+        </a>: <div className="navlist">
           <ul>
             <li>
               <Link to="/">Home</Link>
@@ -38,7 +27,21 @@ const Layout = ({cartItems}) => {
               <Link to='/cart'>Cart ({cartItems})</Link>
             </li>
           </ul>
-        </div>
+        </div> }
+        {dropdown && <div className="navlist">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to='/currencies' >Currencies</Link>
+            </li>
+            <li>
+              <Link to='/cart'>Cart ({cartItems})</Link>
+            </li>
+          </ul>
+        </div>}
+        
         
       </nav>
 
